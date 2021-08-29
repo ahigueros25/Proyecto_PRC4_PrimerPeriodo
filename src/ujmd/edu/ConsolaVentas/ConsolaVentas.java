39932b0e578c32122126028c7e1c099a653da3cd
rjchachagua <201800169@ujmd.edu.sv>
2021-08-29 12:44:38 -0600

    Se realizo el funcionamiento de login

---
+++ src/ujmd/edu/ConsolaVentas/ConsolaVentas.java
@@ -0,0 +1,13 @@
+package ujmd.edu.ConsolaVentas;
+
+import java.util.Scanner;
+import ujmd.edu.clases.usuarios;
+
+public class ConsolaVentas {
+
+    public static void main(String[] args) {
+
+        usuarios usuarios1 = new usuarios();
+        usuarios1.inicializar();
+    }
+}
